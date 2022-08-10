febde8aea6bfb005c419a7a49e0e6f04dc0e1089
alvarogularte <alvaro.gularte@gmail.com>
2022-08-10 17:33:35 -0300

    new: cria conteúdo da aula two-way data binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
@@ -12,7 +14,9 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    FormsModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
