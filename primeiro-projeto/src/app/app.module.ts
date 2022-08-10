d2f17f3d95cd3cbdabc912e87c2ae3fd2f977f1b
alvarogularte <alvaro.gularte@gmail.com>
2022-08-10 14:04:33 -0300

    new: cria primeiro component com cli

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule
