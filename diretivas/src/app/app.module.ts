8b003025b134a41a873f8340c7cd4cea4c626698
alvarogularte <alvaro.gularte@gmail.com>
2022-08-11 17:04:41 -0300

    new: cria conteúdo da aula Diretivas: ngSwitchCase e ngSwitchDefault

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DiretivaNgIfComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgIfComponent,
-    TesteComponent
+    TesteComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
