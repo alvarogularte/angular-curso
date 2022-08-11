ee919fb7dc0cd96aad2ac124ab54f21dc90f5679
alvarogularte <alvaro.gularte@gmail.com>
2022-08-11 10:33:51 -0300

    new: cria conteúdo da aula Angular CLI: Criando components, services: ng generate

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgIfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
