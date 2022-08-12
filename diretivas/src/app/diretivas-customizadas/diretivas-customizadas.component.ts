708f880b9f52fa8cdd17218f3b41297849c2fd78
alvarogularte <alvaro.gularte@gmail.com>
2022-08-12 11:15:00 -0300

    new: cria conteúdo da aula Criando uma diretiva de atributo: ElementRef e Renderer

---
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-customizadas',
+  templateUrl: './diretivas-customizadas.component.html',
+  styleUrls: ['./diretivas-customizadas.component.scss']
+})
+export class DiretivasCustomizadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('');
+  }
+
+}
