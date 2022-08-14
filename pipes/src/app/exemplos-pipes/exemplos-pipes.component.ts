a341f3f3b99a2062bbdf6f85cb09cd0090871014
alvarogularte <alvaro.gularte@gmail.com>
2022-08-14 19:50:03 -0300

    new: cria conteúdo da aula Pipes: Async(Assíncrono)

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -19,6 +20,12 @@ export class ExemplosPipesComponent implements OnInit {
   livros: string[] = ['Java', 'Angular 2'];
 
   filtro: string = '';
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = interval(2000).pipe().subscribe(valor => 'Valor assíncrono 2')
 
   constructor() { }
 
