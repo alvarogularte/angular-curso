1e967c8fce9f5eb0df7eac5aeb1d4e54c58b3463
alvarogularte <alvaro.gularte@gmail.com>
2022-08-14 19:34:19 -0300

    new: cria conteúdo da aula Pipes: Criando um Pipe Impuro

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -29,4 +29,19 @@ export class ExemplosPipesComponent implements OnInit {
     this.livros.push(valor)
   }
 
+  obterCursos() {
+    if (this.livros.length === 0 || this.filtro === undefined
+      || this.filtro.trim() === '') {
+      return this.livros;
+    }
+
+    return this.livros.filter((v: string) => {
+        if (v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0) {
+          return true;
+        }
+
+        return false;
+      });
+  }
+
 }
