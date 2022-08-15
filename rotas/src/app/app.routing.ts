8942144ab95235ebc3a67c6db7d1638a7d64ac29
alvarogularte <alvaro.gularte@gmail.com>
2022-08-15 08:17:30 -0300

    new: cria conteúdo da aula Rotas: Configurando rotas simples

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CursosComponent } from "./cursos/cursos.component";
+
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+
+const APP_ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'cursos', component: CursosComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+
+// Achei aqui: https://stackoverflow.com/questions/62755093/angular-error-generic-type-modulewithproviderst-requires-1-type-arguments
+// Como corrigir erro de tipo do ModuleWithProviders que agora exige um tipo generico
+declare module "@angular/core" {
+  interface ModuleWithProviders<T = any> {
+      ngModule: Type<T>;
+      providers?: Provider[];
+  }
+}
