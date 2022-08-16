585a46e32051a9d8e83cf0534b539f7be38868eb
alvarogularte <alvaro.gularte@gmail.com>
2022-08-16 11:19:21 -0300

    new: cria conteúdo da aula Usando Guarda de Rotas: CanDeactivate com Interface Genérica

--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { IFormCanDeactivate } from 'src/app/guards/iform-candeactivate';
 import { AlunosService } from '../alunos.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { AlunosService } from '../alunos.service';
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, IFormCanDeactivate {
 
   aluno: any;
   inscricao!: Subscription;
