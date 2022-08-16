8214a5c30eae92b546993144a48a3d5861230179
alvarogularte <alvaro.gularte@gmail.com>
2022-08-16 09:33:27 -0300

    new: cria conteúdo das aulas Rotas: Tela de Login e como não mostrar o Menu e Usando Guarda de Rotas: CanActivate

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app.routing.module';
+import { AuthService } from './login/auth.service';
+import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
@@ -17,10 +20,14 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     NgbModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -3,18 +3,23 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 // Mudança na forma de utilizar loadChildren -- fonte: https://stackoverflow.com/questions/70313032/type-string-is-not-assignable-to-type-loadchildrencallback
 
 const APP_ROUTES: Routes = [
   { path: 'cursos',
     loadChildren: () => import('../app/cursos/cursos.module')
-      .then(x => x.CursosModule)},
+      .then(x => x.CursosModule),
+    canActivate: [AuthGuard]
+    },
   { path: 'alunos',
     loadChildren: () => import('../app/alunos/alunos.module')
-      .then(x => x.AlunosModule)},
+      .then(x => x.AlunosModule),
+    canActivate: [AuthGuard]
+    },
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
