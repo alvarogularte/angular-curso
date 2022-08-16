f5bddd816633b859b8e951b46696ee5266abe46a
alvarogularte <alvaro.gularte@gmail.com>
2022-08-16 16:08:37 -0300

    new: cria conteúdo da aula Forms(template driven) Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 
   constructor() { }
 
