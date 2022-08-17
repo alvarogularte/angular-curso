c15f634650cc0e4975bdb7753bbee1f7201b73e9
alvarogularte <alvaro.gularte@gmail.com>
2022-08-17 17:06:12 -0300

    new: cria conteúdo da aula Formulários reativos:Autopopulando endereço com CEP

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -72,4 +72,54 @@ export class DataFormComponent implements OnInit {
     }
   }
 
+  // consultaCEP() {
+
+  //   let cep = this.formulario.get('endereco.cep')['controls'];
+
+  //   cep = cep!.replace(/\D/g, '');
+  //   console.log('logando depois do replace', cep);
+
+  //   if (cep != "") {
+  //     const validacep = /^[0-9]{8}$/;
+  //     console.log('logando validacep', validacep);
+
+
+  //     if (validacep.test(cep)) {
+
+  //       this.resetaDadosForm();
+
+  //       this.http.get(`//viacep.com.br/ws/${cep}/json`)
+  //         .subscribe(dados => this.populaDadosForm(dados))
+  //     }
+  //   }
+  // }
+
+  populaDadosForm(dados: any) {
+
+    this.formulario.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+
+    // this.formulario.get('nome')?.setValue('Álvaro');
+  }
+
+  resetaDadosForm() {
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
+  }
+
 }
