f6915cb3111d2c6bb300c700d1ceaa2a10ec60d6
alvarogularte <alvaro.gularte@gmail.com>
2022-08-17 10:46:46 -0300

    new: cria conteúdo da aula Forms (template driven) Pesquisando endereço automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -27,7 +29,24 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  constructor() { }
+  consultaCEP(cep: any) {
+    cep = cep.replace(/\D/g, '');
+    console.log('logando depois do replace', cep);
+
+    if (cep != "") {
+      const validacep = /^[0-9]{8}$/;
+      console.log('logando validacep', validacep);
+
+
+      if (validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .subscribe(dados => console.log(dados))
+      }
+    }
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
