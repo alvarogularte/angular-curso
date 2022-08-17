33f5d188d6b765c934e5968c45c9e2a8b2d07aef
alvarogularte <alvaro.gularte@gmail.com>
2022-08-17 14:35:18 -0300

    new: cria conteúdo da aula Formulários reativos: Fazendo submit

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -31,8 +31,16 @@ export class DataFormComponent implements OnInit {
 
   onSubmit() {
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-      .subscribe(dados => console.log(dados)
-      );
+      .subscribe(dados => {
+        console.log(dados);
+
+        this.resetar();
+      },
+      (error: any) => alert(`deu ruim ${error}`));
+  }
+
+  resetar() {
+    this.formulario.reset();
   }
 
 }
