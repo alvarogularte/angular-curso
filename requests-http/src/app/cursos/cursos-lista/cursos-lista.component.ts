d2a4903b39a8e16a05ec15836a9708943fdeab9d
alvarogularte <alvaro.gularte@gmail.com>
2022-08-19 14:36:23 -0300

    new: cria conteúdo da aula Capturando Erros (+ Erro com async)

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable, Subject } from 'rxjs';
 import { Curso } from '../cursos';
 import { CursosService } from '../cursos.service';
 
@@ -12,11 +12,29 @@ import { CursosService } from '../cursos.service';
 export class CursosListaComponent implements OnInit {
 
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
-    this.cursos$ = this.service.list();
+    this.onRefresh();
   }
 
+  onRefresh() {
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY
+      })
+    );
+    // this.service.list().subscribe(
+    //   dados => {
+    //     console.log(dados)
+    //   },
+    //   error => console.error(error),
+    //   () => console.log('Observable completo')
+    // )
+  }
 }
